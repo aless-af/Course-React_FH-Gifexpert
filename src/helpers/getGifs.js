e3e640062e8c10e4a0fea2e684cc8f19809f6360
Alex Antelo <77730609+aless-af@users.noreply.github.com>
2023-04-04 10:44:27 +0200

    init

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+export const getGifs = async( category ) => {
+
+    const apiKey = '3Fh261W0oCuWwM4UMy2MmxRDAqWKZB9F'
+
+    const limit = 10;
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=${limit}`
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+}))
+
+return gifs;
+
+}
